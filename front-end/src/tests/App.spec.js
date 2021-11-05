a43b5a678d1c48ef6236045f689fedf3de7a4f85
Gabriel <g.pereira.a.m@gmail.com>
2021-11-05 11:41:07 -0300

    feat(Front-end) - :sparkle Testes do Front-end

--- front-end/src/tests/App.spec.js
+++ front-end/src/tests/App.spec.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {
+  render, screen,
+} from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const heading = screen.getByText('Olá');
-  expect(heading).toBeInTheDocument();
+describe('Verificando se os elementos estão na tela', () => {
+  test('Verificando se a aplicação possui um título', () => {
+    render(<App />);
+    const message = 'To Do List';
+    const title = screen.getByText(message);
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Verificando se existe o campo de digitar a tarefa', () => {
+    render(<App />);
+    const idMessage = 'input-test';
+    const inputTask = screen.getByTestId(idMessage);
+    expect(inputTask).toBeInTheDocument();
+  });
+
+  test('Verificando se existe um botão para criar a tarefa', () => {
+    render(<App />);
+    const message = 'Adicionar Tarefa';
+    const btnMessage = screen.getByText(message);
+    expect(btnMessage).toBeInTheDocument();
+  });
 });
+
+// describe('', () => {});
